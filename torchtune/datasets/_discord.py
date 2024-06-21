4c9596d6ae9e97004924c225b4ef9c1d44cab267
Josef Kuchař <josef.kuchar267@gmail.com>
2024-06-21 11:22:55 +0200

    Discord finetunning works! kindof

--- torchtune/datasets/_discord.py
+++ torchtune/datasets/_discord.py
@@ -1,20 +1,9 @@
-from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple
-
-import numpy as np
-
+from typing import Any, Dict, List, Mapping, Tuple
 from datasets import load_dataset
 from torch.utils.data import Dataset
-from torchtune.config._utils import _get_chat_format
-from torchtune.data import (
-    ChatFormat,
-    CROSS_ENTROPY_IGNORE_IDX,
-    Message,
-    openai_to_llama2_messages,
-    sharegpt_to_llama2_messages,
-    validate_messages,
-)
 from torchtune.modules.tokenizers import Tokenizer
-
+import json
+from tqdm import tqdm
 
 class DiscordDataset(Dataset):
     def __init__(
@@ -23,17 +12,57 @@ class DiscordDataset(Dataset):
         tokenizer: Tokenizer,
         source: str,
         max_seq_len: int,
-        train_on_input: bool = True,
         **load_dataset_kwargs: Dict[str, Any],
     ) -> None:
         self._tokenizer = tokenizer
-        self._data = load_dataset(
-            "json", data_files="./backrooms.json", field="rooms", split="train", **load_dataset_kwargs
-        )
-        print(len(self._data))
-        exit()
+        with open("./backrooms.json", "r") as f:
+            data = json.load(f)["rooms"]
         self.max_seq_len = max_seq_len
-        self.train_on_input = train_on_input
+        self._data = []
+
+        for room in data:
+            tokenized_messages = []
+            for message in tqdm(room):
+                tokens = []
+                # Start with a special token to indicate the start of a new message
+                tokens += [self._tokenizer._encode_special_token("<|start_header_id|>")]
+                # Message ID
+                tokens += self._tokenizer.encode(message['id'], add_bos=False, add_eos=False)
+                # Separate with another special token
+                tokens += [self._tokenizer._encode_special_token("<|fim_prefix|>")]
+                # Message author
+                tokens += self._tokenizer.encode(message["author"], add_bos=False, add_eos=False)
+                # Separate with another special token
+                tokens += [self._tokenizer._encode_special_token("<|fim_middle|>")]
+                # Reply ID
+                if message["reference"] is not None:
+                    tokens += self._tokenizer.encode(message["reference"], add_bos=False, add_eos=False)
+                # Separate with another special token
+                tokens += [self._tokenizer._encode_special_token("<|end_header_id|>")]
+                # Begin message with new line
+                tokens += self._tokenizer.encode("\n", add_bos=False, add_eos=False)
+                # Message content
+                tokens += self._tokenizer.encode(message["content"], add_bos=False, add_eos=False)
+                # End message with special token and double new line
+                tokens += [self._tokenizer._encode_special_token("<|eom_id|>")]
+                tokens += self._tokenizer.encode("\n\n", add_bos=False, add_eos=False)
+                tokenized_messages.append(tokens)
+            current_chunk = [self._tokenizer._encode_special_token("<|begin_of_text|>")]
+            i = 0
+            while True:
+                if i == len(tokenized_messages) or len(current_chunk) + len(tokenized_messages[i]) > self.max_seq_len - 1:
+                    current_chunk += [self._tokenizer._encode_special_token("<|end_of_text|>")]
+                    self._data.append(current_chunk)
+                    current_chunk = [self._tokenizer._encode_special_token("<|begin_of_text|>")]
+                    if i == len(tokenized_messages):
+                        break
+                    # Create overlap between chunks
+                    i -= 4
+                else:
+                    current_chunk += tokenized_messages[i]
+                    i += 1
+        print(f"Number of chunks: {len(self._data)}")
+
 
     def __len__(self):
         return len(self._data)
@@ -43,26 +72,16 @@ class DiscordDataset(Dataset):
         return self._prepare_sample(sample)
 
     def _prepare_sample(self, sample: Mapping[str, Any]) -> Tuple[List[int], List[int]]:
-        # messages = self._convert_to_messages(sample, self.train_on_input)
-        # if self.chat_format is not None:
-        #     messages = self.chat_format.format(messages)
-        # validate_messages(messages)
-        # tokens, mask = self._tokenizer.tokenize_messages(
-        #     messages, max_seq_len=self.max_seq_len
-        # )
-        # # Wherever mask == True, set to CROSS_ENTROPY_IGNORE_IDX. Otherwise keep as tokens
-        # labels = list(np.where(mask, CROSS_ENTROPY_IGNORE_IDX, tokens))
-        # assert len(tokens) == len(labels)
+        labels = sample.copy()
 
-        return [], []
+        return { "tokens": sample, "labels": labels }
 
 def discord_dataset(
     tokenizer: Tokenizer,
     source: str = "liweili/c4_200m",
-    train_on_input: bool = False
 ):
     return DiscordDataset(
         tokenizer=tokenizer,
         source=source,
-        train_on_input=train_on_input,
+        max_seq_len=4096,
     )
